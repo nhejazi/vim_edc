068ac9ca19fde7031728ba8b87db9574cb3fcdde
Nima Hejazi <nima.hejazi7@gmail.com>
2016-01-27 21:01:53 -0800

    update setup

--- _setup.sh
+++ _setup.sh
@@ -1,6 +1,13 @@
 cd
 git clone http://github.com/nhejazi/myvimconfig.git ~/.vim
+
+if [ -e ~/.vimrc]
+	then
+		echo "vimrc dotfile exists - will be removed."
+		rm ~/.vimrc
+fi
 ln -s ~/.vim/vimrc ~/.vimrc
+
 cd ~/.vim
 git submodule init
 git submodule update
