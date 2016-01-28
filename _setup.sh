efe7a7cc4a9aa2ba8faf707dd1d54896f8717bfb
Nima Hejazi <nima.hejazi7@gmail.com>
2016-01-28 15:12:51 -0800

    Update _setup.sh

--- _setup.sh
+++ _setup.sh
@@ -1,10 +1,9 @@
 cd
 git clone http://github.com/nhejazi/myvimconfig.git ~/.vim
 
-if [ -e ~/.vimrc]
-	then
-		echo "vimrc dotfile exists - will be removed."
-		rm ~/.vimrc
+if [ -e ~/.vimrc]; then
+	echo "vimrc dotfile exists - will be removed.";
+	rm ~/.vimrc;
 fi
 ln -s ~/.vim/vimrc ~/.vimrc
 
