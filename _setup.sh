7f085adc43e48e65da95c6c610e778614f95bfea
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-05 11:29:40 -0700

    add neovimrc setup

--- _setup.sh
+++ _setup.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
 
-# set up Vim customization files
-if [ -e ~/.vimrc]; then
+# set up Vim customization file
+if [ -e ~/.vimrc ]; then
 	echo "vimrc dotfile exists - will be removed.";
 	rm ~/.vimrc;
 fi
 ln -s ~/.vim/vimrc ~/.vimrc
+
+# set up NeoVim customization file
+if [ -e ~/.nvimrc ]; then
+	echo "nvimrc dotfile exists - will be removed.";
+	rm ~/.nvimrc;
+fi
+ln -s ~/.vim/nvimrc ~/.nvimrc
 
 cd ~/.vim
 git submodule init
